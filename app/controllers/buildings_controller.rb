26c5a6dfc1f4a5ccea41f2e01e14544c77ff4e61
Ryan Wick <ryanwick@gmail.com>
2013-09-16 15:23:16 -0700

    Expanded Buildings views and incorporated images. Added header back in.

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,7 +1,15 @@
 class BuildingsController < ApplicationController
   def index
+    @title = "Building List"
+
+    @buildings = Buildings.list
   end
 
   def show
+    @building = Buildings.find(params[:id])
+    
+    # flickr ID as constant?
+
+    @title = @building.name
   end
 end
--- app/models/buildings.rb
+++ app/models/buildings.rb
@@ -1,3 +1,5 @@
 class Buildings < ActiveRecord::Base
   attr_accessible :description, :flickr_orig_url, :flickr_tag, :flickr_thumb_url, :lat, :lng, :name, :search_tags
+
+  scope :list, :order => :name
 end
