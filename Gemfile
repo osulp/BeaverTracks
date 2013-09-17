2364d7d879dcfd0e96059f425157d57c83cfb2c6
Ryan Wick <ryanwick@gmail.com>
2013-09-17 16:41:03 -0700

    Added Flickraw-cached gem. Got views on Buildings pages pulling in tagged photos. Put API Key and ID in environment variables to keep out of repo.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jquery-rails'
 
 gem 'rails_admin'
 
+gem 'flickraw-cached'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,6 @@
 class BuildingsController < ApplicationController
+
+
   def index
     @title = "Building List"
 
@@ -7,10 +9,20 @@ class BuildingsController < ApplicationController
 
   def show
     @building = Buildings.find(params[:id])
-    
-    # flickr ID as constant?
 
     @title = @building.name
+
+
+    require 'flickraw'
+
+    FlickRaw.api_key = ENV["BEAVERTRACKS_FLICKR_API_KEY"]
+    FlickRaw.shared_secret = ENV["BEAVERTRACKS_FLICKR_SECRET"]
+
+    @flickr_user_id = ENV["BEAVERTRACKS_FLICKR_USER_ID"]
+
+    @photos = flickr.photos.search :user_id => @flickr_user_id, :tags => @building.flickr_tag
+
+    @photos = @photos.sort_by {|a| a.title}
   end
 
   def image
