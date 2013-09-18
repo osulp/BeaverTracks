db7939e7406661273b3f5084fa309e407c248fed
Ryan Wick <ryanwick@gmail.com>
2013-09-18 15:50:46 -0700

    Disabled smaller header display on homepage.

--- app/controllers/beavertracks_controller.rb
+++ app/controllers/beavertracks_controller.rb
@@ -1,4 +1,14 @@
 class BeavertracksController < ApplicationController
   def index
+    @title = 'Home'
+    @disable_header = 'true'
+  end
+
+  def about
+    @title = 'About'
+  end
+
+  def instructions
+    @title = 'About Tour'
   end
 end
