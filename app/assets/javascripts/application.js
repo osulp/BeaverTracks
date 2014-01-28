e50c47749c88d0665f4d492157e067c49c863d32
Ryan Wick <ryanwick@gmail.com>
2014-01-27 17:52:27 -0800

    Started updating gmaps4rails code on views and controller to v2 version.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 
 //= require underscore-min
 //= require gmaps/google
+
+//= require_tree .
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -11,8 +11,6 @@ class MapsController < ApplicationController
       marker.title building.name
     end
 
-
-
 #    @json = Buildings.all.to_gmaps4rails do |building, marker|
 #      marker.json({ :id => building.id, :title => building.name })
 #      marker.infowindow render_to_string(:partial => "maps/infowindow", :locals => { :building => building })
@@ -28,24 +26,31 @@ class MapsController < ApplicationController
 
     @title = "Nearby " + @building.name
 
-    # set building markers, different one for selected building
-    @json = Buildings.nearby(@building).to_gmaps4rails do |building, marker|
-      marker.json({ :id => building.id, :title => building.name })
-      marker.infowindow render_to_string(:partial => "maps/infowindow", :locals => { :building => building })
-
-      if @building.id === building.id
-        marker.picture({
-          "picture" => ActionController::Base.helpers.asset_path('arrow.png'),
-          "width" => 32,
-          "height" => 37,
-          "marker_anchor" => [5, 10],
-          "shadow_picture" => ActionController::Base.helpers.asset_path('shadow.png'),
-          "shadow_width" => 51,
-          "shadow_height" => 37,
-          "shadow_anchor" => [5, 10]
-        })
-      end
+    @hash = Gmaps4rails.build_markers(Buildings.nearby(@building)) do |building, marker|
+      marker.lat building.lat
+      marker.lng building.lng
+      marker.title building.name
     end
+
+
+    # set building markers, different one for selected building
+#    @json = Buildings.nearby(@building).to_gmaps4rails do |building, marker|
+#      marker.json({ :id => building.id, :title => building.name })
+#      marker.infowindow render_to_string(:partial => "maps/infowindow", :locals => { :building => building })
+
+#      if @building.id === building.id
+#        marker.picture({
+#          "picture" => ActionController::Base.helpers.asset_path('arrow.png'),
+#          "width" => 32,
+#          "height" => 37,
+#          "marker_anchor" => [5, 10],
+#          "shadow_picture" => ActionController::Base.helpers.asset_path('shadow.png'),
+#          "shadow_width" => 51,
+#          "shadow_height" => 37,
+#          "shadow_anchor" => [5, 10]
+#        })
+#      end
+#    end
 
   end
 
@@ -58,31 +63,39 @@ class MapsController < ApplicationController
 
     @patron = Patron.from_coords(session)
 
-    # set markers, walking icon for patron location
-    @json = (Buildings.nearby(@patron) | [@patron]).to_gmaps4rails do |building, marker|
-      if building.kind_of? Buildings
-        marker.json({ :id => building.id, :title => building.name })
-        marker.infowindow render_to_string(:partial => "maps/building_tour_infowindow", :locals => { :building => building })
-      end
-
-      if building.kind_of? Patron
-        marker.json({ :id => building.id, :title => "You" })
-
-        marker.picture({
-          "picture" => ActionController::Base.helpers.asset_path('walking.png'),
-          "width" => 32,
-          "height" => 37,
-          "marker_anchor" => [5, 10],
-          "shadow_picture" => ActionController::Base.helpers.asset_path('shadow.png'),
-          "shadow_width" => 51,
-          "shadow_height" => 37,
-          "shadow_anchor" => [5, 10]
-        })
-
-        marker.infowindow render_to_string(:partial => "maps/patron_tour_infowindow", :locals => { :building => building })
-      end
-
+    @hash = Gmaps4rails.build_markers(Buildings.all) do |building, marker|
+      marker.lat building.lat
+      marker.lng building.lng
+#      marker.id building.id
+      marker.title building.name
     end
+
+
+    # set markers, walking icon for patron location
+#    @json = (Buildings.nearby(@patron) | [@patron]).to_gmaps4rails do |building, marker|
+#      if building.kind_of? Buildings
+#        marker.json({ :id => building.id, :title => building.name })
+#        marker.infowindow render_to_string(:partial => "maps/building_tour_infowindow", :locals => { :building => building })
+#      end
+#
+#      if building.kind_of? Patron
+#        marker.json({ :id => building.id, :title => "You" })
+#
+#        marker.picture({
+#          "picture" => ActionController::Base.helpers.asset_path('walking.png'),
+#          "width" => 32,
+#          "height" => 37,
+#          "marker_anchor" => [5, 10],
+#          "shadow_picture" => ActionController::Base.helpers.asset_path('shadow.png'),
+#          "shadow_width" => 51,
+#          "shadow_height" => 37,
+#          "shadow_anchor" => [5, 10]
+#        })
+#
+#        marker.infowindow render_to_string(:partial => "maps/patron_tour_infowindow", :locals => { :building => building })
+#      end
+#
+#    end
 
     @on_campus = session[:campus]
 
