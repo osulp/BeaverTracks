8d898ec885175dfba2957a560d59f2af5cfcb93f
Ryan Wick <ryanwick@gmail.com>
2014-02-21 12:55:00 -0800

    Removed basically empty test directory.

--- test/unit/buildings_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BuildingsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/beavertracks_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BeavertracksHelperTest < ActionView::TestCase
-end
