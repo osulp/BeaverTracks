ca8e3f54c14adb1f6d65bb30bb2f350d79d6f68a
Ryan Wick <ryanwick@gmail.com>
2014-02-25 14:23:50 -0800

    Changed Patron model to use OpenStruct.

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -1,14 +1,7 @@
-class Patron < ActiveRecord::Base
-  attr_accessible :city, :country, :distance, :lat, :lng, :locality, :state, :street, :zip
-
-  geocoded_by :address, :latitude => :lat, :longitude => :lng
+class Patron < OpenStruct
 
   def self.from_coords(opts={})
     new(:lat => opts[:lat], :lng => opts[:lng])
-  end
-
-  def address
-      [street, city, state, zip, country].compact.join(', ')
   end
 
 end
