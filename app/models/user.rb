81d36fa28b29704d12dc21e203b560888c996e0f
Ryan Wick <ryanwick@gmail.com>
2014-05-22 11:55:23 -0700

    Adding attributes back to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
