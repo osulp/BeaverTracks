bc624580cf19d9830b840ed72bceadd32fc72a4e
Ryan Wick <ryanwick@gmail.com>
2014-05-22 15:31:05 -0700

    Added rails_env as staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,6 +13,7 @@ set :branch, config['branch']
 set (:god_app_path) {"#{current_path}/#{config['god']['app_path']}"}
 set :god_sites_path,  config['god']['sites_path']
 set :deploy_to, config['deploy_to']
+set :rails_env, :staging
 # Deploy Commands
 # Override deploy to inform god to do the restarts.
 namespace :deploy do
